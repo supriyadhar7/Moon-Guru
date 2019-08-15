6bf8427cccf668cd93b18064ef3efc1e37d7108a
Apple <apple@Apple-Computer.local>
2019-08-15 11:43:31 -0400

    goal2

--- main.js
+++ main.js
@@ -16,24 +16,70 @@ var button15 = $('.15');
 var buttonYes = $('.Yes');
 var buttonNo = $('.No');
 var buttonSkip = $('.Skip');
-var medicine = $('.medicine')
+var medicine = $('.medicine');
 
 buttonYes.on("click", displayAdviceYes);
 
 function displayAdviceYes() {
-  medicine.append("");
+  mental.append("");
   //next page
 }
 
 buttonNo.on("click", displayAdviceNo);
 
 function displayAdviceNo() {
-  medicine.append("");
+  mental.append("");
   //next page
 }
 
 buttonSkip.on("click", displayAdviceSkip)
 
 function displayAdviceSkip() {
-  medicine.append("");
+  physical.append("");
 }
+
+button1.on("click", displayAdvice1);
+
+function displayAdvice1() {
+  physical.append("");
+}
+
+button2.on("click", displayAdvice2);
+
+function displayAdvice2() {
+  physical.append("");
+}
+
+button3.on("click", displayAdvice3);
+
+function displayAdvice3() {
+  physical.append("");
+}
+
+button4.on("click", displayAdvice4);
+
+function displayAdvice4() {
+  physical.append("");
+}
+
+button5.on("click", displayAdvice5);
+
+function displayAdvice5() {
+  spiritual.append("");
+}
+
+button6.on("click", displayAdvice6);
+
+function displayAdvice6() {
+  spiritual.append("");
+}
+
+button7.on("click", displayAdvice7);
+
+function displayAdvice7() {
+  spiritual.append("");
+}
+
+button8.on("click", displayAdvice8);
+
+function displayAdvice
