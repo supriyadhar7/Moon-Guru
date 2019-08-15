6ba2921d30fe721a8f06f9260eadd985dc6f403e
Apple <apple@Apple-Computer.local>
2019-08-15 16:01:50 -0400

    4pm

--- main.js
+++ main.js
@@ -32,8 +32,9 @@ buttonYes.on("click", displayAdviceYes);
 
 function displayAdviceYes() {
   mental.css('display', 'inline');
+  buttonYes.css('background-color', 'pink');
   mental.append(`<h3>Mental</h3>`);
-  mental.append(`<img src ="http://dmec.org/wp-content/uploads/Mental-Well-Being-Resources-300x275.png">`);
+  mental.append(`<img class ="mentalPic" src ="http://dmec.org/wp-content/uploads/Mental-Well-Being-Resources-300x275.png">`);
   mental.append(`<p><strong>Be kind to your mind.</strong><p>`);
   mental.append(`<p>If you or someone you know has a mental illness, there are ways to get help.</p><p> Use these resources to find help for you, a friend, or a family member.</p>`);
 
@@ -47,8 +48,9 @@ buttonNo.on("click", displayAdviceNo);
 
 function displayAdviceNo() {
   mental.css('display', 'inline');
+  buttonNo.css('background-color', 'pink');
   mental.append(`<h3>Mental</h3>`);
-  mental.append(`<img src ="http://dmec.org/wp-content/uploads/Mental-Well-Being-Resources-300x275.png">`);
+  mental.append(`<img class="mentalPic" src ="http://dmec.org/wp-content/uploads/Mental-Well-Being-Resources-300x275.png">`);
   //next page
 }
 
@@ -56,31 +58,40 @@ buttonSkip.on("click", displayAdviceSkip)
 
 function displayAdviceSkip() {
   mental.css('display', 'inline');
+  buttonSkip.css('background-color', 'pink');
   mental.append(`<h3>Mental</h3>`);
-  mental.append(`<img src ="http://dmec.org/wp-content/uploads/Mental-Well-Being-Resources-300x275.png">`);
+  mental.append(`<img class="mentalPic" src ="http://dmec.org/wp-content/uploads/Mental-Well-Being-Resources-300x275.png">`);
 }
 
 button1.on("click", displayAdvice1);
 
 function displayAdvice1() {
   physical.css('display', 'inline');
+  button1.css('background-color', 'pink');
   physical.append(`<h3>Physical</h3>`);
+  physical.append(`<img class="physicalPic" src ="https://www.liderazgohoy.com/wp-content/uploads/2016/02/sunset-summer-golden-hour-paul-filitchkin-1024x768.jpg">`);
   physical.append(`<p>Set small goals for yourself, like going for a walk! Make it fun and get creative.<p>`);
+
 }
 
 button2.on("click", displayAdvice2);
 
 function displayAdvice2() {
   physical.css('display', 'inline');
+  button2.css('background-color', 'pink');
   physical.append(`<h3>Physical</h3>`);
+  physical.append(`<img class="physicalPic" src ="https://www.liderazgohoy.com/wp-content/uploads/2016/02/sunset-summer-golden-hour-paul-filitchkin-1024x768.jpg">`);
   physical.append(`<p>Keep it up! Try incorporating new fun and hip workout trends into your routine.</p>`);
+
 }
 
 button3.on("click", displayAdvice3);
 
 function displayAdvice3() {
   physical.css('display', 'inline');
+  button3.css('background-color', 'pink');
   physical.append(`<h3>Physical</h3>`);
+  physical.append(`<img class="physicalPic" src ="https://www.liderazgohoy.com/wp-content/uploads/2016/02/sunset-summer-golden-hour-paul-filitchkin-1024x768.jpg">`);
   physical.append(`<p>You're doing great! With all of this exercise, make sure you're staying hydrated and getting all of the proper nutrients.</p>`);
 }
 
@@ -88,7 +99,9 @@ button4.on("click", displayAdvice4);
 
 function displayAdvice4() {
   physical.css('display', 'inline');
+  button4.css('background-color', 'pink');
   physical.append(`<h3>Physical</h3>`);
+  physical.append(`<img class="physicalPic" src ="https://www.liderazgohoy.com/wp-content/uploads/2016/02/sunset-summer-golden-hour-paul-filitchkin-1024x768.jpg">`);
   physical.append(`<p>You're doing great! With all of this exercise, make sure you're staying hydrated and getting all of the proper nutrients</p>`);
 }
 
@@ -96,6 +109,7 @@ button5.on("click", displayAdvice5);
 
 function displayAdvice5() {
   spiritual.css('display', 'inline');
+  button5.css('background-color', 'pink');
   spiritual.append(`<h3>Spiritual</h3>`);
   spiritual.append();
 }
@@ -104,6 +118,7 @@ button6.on("click", displayAdvice6);
 
 function displayAdvice6() {
   spiritual.css('display', 'inline');
+  button6.css('background-color', 'pink');
   spiritual.append(`<h3>Spiritual</h3>`);
   spiritual.append();
 }
@@ -112,6 +127,7 @@ button7.on("click", displayAdvice7);
 
 function displayAdvice7() {
   spiritual.css('display', 'inline');
+  button7.css('background-color', 'pink');
   spiritual.append(`<h3>Spiritual</h3>`);
   spiritual.append();
 }
@@ -120,6 +136,7 @@ button8.on("click", displayAdvice8);
 
 function displayAdvice8() {
   spiritual.css('display', 'inline');
+  button8.css('background-color', 'pink');
   spiritual.append(`<h3>Spiritual</h3>`);
   spiritual.append();
 }
@@ -128,6 +145,7 @@ button9.on("click", displayAdvice9);
 
 function displayAdvice9() {
   emotional.css('display', 'inline');
+  button9.css('background-color', 'pink');
   emotional.append(`<h3>Emotional</h3>`);
   emotional.append();
 }
@@ -136,6 +154,7 @@ button10.on("click", displayAdvice10);
 
 function displayAdvice10() {
   emotional.css('display', 'inline');
+  button10.css('background-color', 'pink');
   emotional.append(`<h3>Emotional</h3>`);
   emotional.append();
 }
@@ -144,14 +163,16 @@ button11.on("click", displayAdvice11);
 
 function displayAdvice11() {
   emotional.css('display', 'inline');
+  button11.css('background-color', 'pink');
   emotional.append(`<h3>Emotional</h3>`);
-  emotional.append(`<p>When you're feeling sad, it can be beneficial to take note of everything you're grateful for. Try starting a gratitude journal</p>`);
+  emotional.append(`<p>When you're feeling sad, it can be beneficial to take note of everything you're grateful for. Try starting a gratitude journal.</p>`);
 }
 
 button12.on("click", displayAdvice12);
 
 function displayAdvice12() {
   emotional.css('display', 'inline');
+  button12.css('background-color', 'pink');
   emotional.append(`<h3>Emotional</h3>`);
   emotional.append();
 }
@@ -160,6 +181,7 @@ button13.on("click", displayAdvice13);
 
 function displayAdvice13() {
   environmental.css('display', 'inline');
+  button13.css('background-color', 'pink');
   environmental.append(`<h3>Environmental</h3>`);
   environmental.append();
 }
@@ -168,6 +190,7 @@ button14.on("click", displayAdvice14);
 
 function displayAdvice14() {
   environmental.css('display', 'inline');
+  button14.css('background-color', 'pink');
   environmental.append(`<h3>Environmental</h3>`);
   environmental.append();
 }
@@ -176,6 +199,7 @@ button15.on("click", displayAdvice15);
 
 function displayAdvice15() {
   environmental.css('display', 'inline');
+  button15.css('background-color', 'pink');
   environmental.append(`<h3>Environmental</h3>`);
   environmental.append();
 }
